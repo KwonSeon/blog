ee3bae6a25a2424fd6029ed9202a553766efbbb6
KWON SEON <ksun2128@pukyong.ac.kr>
2026-02-22 18:24:34 +0900

    [P0-006-BE-1][prep] Spring API 기본 프로젝트 초기화

---
+++ apps/api/src/test/java/com/snowk/blog/api/ApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.snowk.blog.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
